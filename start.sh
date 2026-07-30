442946c114288f64994437986d5721b96af04c90
aquamxrinee <164550952+aquamxrinee@users.noreply.github.com>
2026-07-30 17:46:58 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,5 @@
 echo "Установка зависимостей..."
 pip install -r requirements.txt
 echo "Установка браузеров для Playwright..."
-playwright install chromium
-playwright install-deps
 echo "Запуск бота..."
 python main.py
