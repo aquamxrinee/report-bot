61057cda72cefe32ba79a80e32e9c5556fcf5ec5
aquamxrinee <164550952+aquamxrinee@users.noreply.github.com>
2026-08-01 03:40:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,8 @@ def main():
     app.add_handler(CommandHandler("spp_stats", spp_stats_cmd))
     app.add_handler(CommandHandler("test_parser", test_parser_cmd))
     app.add_handler(CommandHandler("test_proxy", test_proxy_cmd))
-    app.add_handler(CommandHandler("sync_articles", sync_articles_cmd))   # <-- ДОБАВЛЕНО
+    app.add_handler(CommandHandler("sync_articles", sync_articles_cmd))
+    app.add_handler(CommandHandler("set_article", set_article_cmd))   # <-- ДОБАВЛЕНО
 
     app.add_handler(CallbackQueryHandler(menu_history_callback, pattern="^menu_history$"))
     app.add_handler(CallbackQueryHandler(menu_analytics_callback, pattern="^menu_analytics$"))
